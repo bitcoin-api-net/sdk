e75188e21d560c04e40da59eb2fe578a7bb63731
“Bogdan_Sh” <bshevchenko12@gmail.com>
2026-02-12 15:10:54 +0200

    Refactor web-client layout and styling; remove Counter component, add PublicPagesLayout for improved structure, and implement global styles with SCSS. Update package dependencies to include normalize.css and sass.

--- apps/web-client/astro.config.mjs
+++ apps/web-client/astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
 
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable Vue to support Vue components.
 	integrations: [vue()],
 	site: "https://bitcoin-api.net",
+	vite: {
+		resolve: {
+			alias: {
+				'@styles': path.resolve(__dirname, 'src/styles'),
+				'@components': path.resolve(__dirname, 'src/components'),
+				'@layouts': path.resolve(__dirname, 'src/layouts'),
+			},
+		},
+	},
 });
