121075cc087c53904daae4de683b81ef4791b27a
“Bogdan_Sh” <bshevchenko12@gmail.com>
2026-02-16 23:37:41 +0200

    Add documentation layout and current price page; refactor localization methods for improved consistency

--- apps/web-client/src/services/i18n.ts
+++ apps/web-client/src/services/i18n.ts
@@ -4,7 +4,7 @@ import { DEFAULT_LOCALE } from 'src/constants';
 export class I18nService {
   constructor() {}
 
-  collectionIdToLocale(collectionId: string): Omit<Locale, typeof DEFAULT_LOCALE> | undefined {
+  localeToPrefix(collectionId: string): Omit<Locale, typeof DEFAULT_LOCALE> | undefined {
     if (!isLocale(collectionId)) return;
     if (collectionId === DEFAULT_LOCALE) return;
     return collectionId;
