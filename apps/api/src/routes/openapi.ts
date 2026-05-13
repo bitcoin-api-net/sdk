32ca7c0370eaf94eeee1e35044660ffef582c3da
“Bogdan_Sh” <bshevchenko12@gmail.com>
2026-05-13 16:09:38 +0300

    Enable OpenAPI download via query parameter

--- apps/api/src/routes/openapi.ts
+++ apps/api/src/routes/openapi.ts
@@ -1,8 +1,21 @@
 import { openApiRepository } from '#src/repositories/openapi.repository.js';
+import { JSONSchemaType } from '@fastify/ajv-compiler/node_modules/ajv/dist/ajv.js';
 import { FastifyInstance } from 'fastify';
 
+export type RequestData = {
+  download?: boolean;
+};
+
+const querySchema: JSONSchemaType<RequestData> = {
+  type: 'object',
+  properties: {
+    download: { type: 'boolean', nullable: true },
+  },
+  required: [],
+};
+
 export default async function (fastify: FastifyInstance) {
-  fastify.route({
+  fastify.route<{ Querystring: RequestData }>({
     method: 'GET',
     url: '/openapi.json',
     schema: {
@@ -10,10 +23,15 @@ export default async function (fastify: FastifyInstance) {
       hide: true, // Hide from the schema itself to avoid recursion
       summary: 'Get OpenAPI specification',
       description: 'Returns the full OpenAPI 3.x specification for this API in JSON format.',
+      querystring: querySchema,
       'x-default-rate-limit': 60,
     },
     handler: async (request, reply) => {
       const schema = openApiRepository.getSchema();
+
+      if (request.query.download) {
+        reply.header('Content-Disposition', 'attachment; filename="openapi.json"');
+      }
 
       reply
         .header('Content-Type', 'application/json; charset=utf-8')
